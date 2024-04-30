e70baf3fbccab3ea3f992142a598c2946a265f1e
SkylordGrove <dggrove@uark.edu>
2024-04-29 23:01:30 -0500

    Added URLs to Repo rendering

--- react-app/src/components/Repo.js
+++ react-app/src/components/Repo.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Repo({repository}) {
     console.log(repository)
     return(
-        <li key={repository.name}>{repository.name}-{repository.language}</li>
+        <li key={repository.name}><a href={repository.html_url} target="_blank" rel="noreferrer">{repository.name}</a>-{repository.language}</li>
     )
 }
 
--- react-app/src/components/pages/Home.js
+++ react-app/src/components/pages/Home.js
@@ -4,22 +4,20 @@ import '../../styles/App.css';
 import '../../styles/Navbar.css';
 import Navbar from '../Navbar';
 import { Button } from 'react-bootstrap';
+//import Data_Pill from '../data/Data_Pill';
 
 function Home() {
     return (
       <div className="App">
         <div className="App-header-background">
           <header className="App-header">
-            <img src={headshot} className="App-logo App-header-item" />
-            <Button className="Button App-header-item" onClick={useButton} >This is a cool Button!</Button>
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Learn React
-            </a>
+            <div className="Headshot-wrapper">
+              <img src={headshot} className="App-logo App-header-item" />
+            </div>
+            <div className="Bio">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </div>
+            
           </header>
         </div>
         <body>
@@ -35,3 +33,15 @@ function useButton() {
 }
 
 export default Home
+
+/*
+            <Button className="Button App-header-item" onClick={useButton} >This is a cool Button!</Button>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Learn React
+            </a>
+*/
